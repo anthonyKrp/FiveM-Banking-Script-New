d35fe7a3c462d1fddc5b75dcceeda9ac4eceb64e
AnthonyKrp <anthgaming2000@gmail.com>
2022-06-19 19:03:13 +0200

    master

--- config.lua
+++ config.lua
@@ -1,35 +1,30 @@
 Config = {}
 
-Config.okokTextUI = false -- true = okokTextUI (I recommend you using this since it is way more optimized than the default ShowHelpNotification) | false = ShowHelpNotification
+Config.okokTextUI = false 
+Config.IBANPrefix = "" 
 
-Config.IBANPrefix = "" -- the prefix of the IBAN
+Config.IBANNumbers = 6 
 
-Config.IBANNumbers = 6 -- How many characters the IBAN has by default
+Config.CustomIBANMaxChars = 10
+Config.CustomIBANAllowLetters = true 
 
-Config.CustomIBANMaxChars = 10 -- How many characters the IBAN can have when changing it to a custom one (on Settings tab)
+Config.IBANChangeCost = 5000 
+Config.PINChangeCost = 1000 
 
-Config.CustomIBANAllowLetters = true -- If the custom IBAN can have letters or only numbers (on Settings tab)
-
-Config.IBANChangeCost = 5000 -- How much it costs to change the IBAN to a custom one (on Settings tab)
-
-Config.PINChangeCost = 1000 -- How much it costs to change the PIN (on Settings tab)
-
-Config.AnimTime = 2 * 1000 -- 2 * 1000 = 2 seconds (ATM animation)
-
-Config.Societies = { -- Which societies have bank accounts
+Config.AnimTime = 2 * 1000 
+Config.Societies = { 
 	"police",
 	"ambulance",
 	"mechanic",
 }
 
-Config.SocietyAccessRanks = { -- Which ranks of the society have access to it
+Config.SocietyAccessRanks = {
 	"boss",
 	"chief",
 }
 
-Config.ShowBankBlips = true -- true = show bank blips on the map | false = don't show blips
-
-Config.BankLocations = { -- to get blips and colors check this: https://wiki.gtanet.work/index.php?title=Blips
+Config.ShowBankBlips = true 
+Config.BankLocations = { 
 	{blip = 500, blipColor = 2, blipScale = 0.7, x = 150.266, y = -1040.203, z = 29.374, blipText = "Bank", BankDistance = 3},
 	{blip = 500, blipColor = 2, blipScale = 0.7, x = -1212.980, y = -330.841, z = 37.787, blipText = "Bank", BankDistance = 3},
 	{blip = 500, blipColor = 2, blipScale = 0.7, x = -2962.582, y = 482.627, z = 15.703, blipText = "Bank", BankDistance = 3},
